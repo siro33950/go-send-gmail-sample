e9214d2f7071390592ae33ca59a46826bbfbfcda
siro33950 <siro33950@gmail.com>
2019-12-12 11:46:55 +0900

    feat: 送信者名とタイトルを日本語対応

--- mail.go
+++ mail.go
@@ -8,7 +8,6 @@ import (
 	"google.golang.org/api/option"
 	"io/ioutil"
 	"log"
-	"strings"
 )
 
 func main() {
@@ -18,18 +17,11 @@ func main() {
 		log.Printf("[ERROR] Failed to process create service: %s", err)
 		return
 	}
+
 	// Fromには偽装したアドレスもしくはそのaliasを指定する
-	temp := []byte("From: hoge@gmal.com\r\n" +
-		"To: fuga@gmail.com\r\n" +
-		"Subject: testSubject\r\n" +
-		"\r\ntestBody")
-	var message gmail.Message
-	message.Raw = base64.StdEncoding.EncodeToString(temp)
-	message.Raw = strings.Replace(message.Raw, "/", "_", -1)
-	message.Raw = strings.Replace(message.Raw, "+", "-", -1)
-	message.Raw = strings.Replace(message.Raw, "=", "", -1)
-	
-	// 偽装したアドレスでAPIを叩く
+	message := createMessage("送信者", "hoge_alias@gmail.com", "fuga@gmail.com", "テストメール", "テスト")
+
+	// meを指定すれば偽装したアカウントでAPIを叩いてくれる
 	_, err = service.Users.Messages.Send("me", &message).Do()
 	if err != nil {
 		log.Printf("[ERROR] Failed to process send message: %s", err)
@@ -58,4 +50,18 @@ func createService() (service *gmail.Service, err error) {
 	ctx := context.Background()
 	tokenSource := config.TokenSource(ctx)
 	return gmail.NewService(ctx, option.WithTokenSource(tokenSource))
+}
+
+func createMessage(senderName string, senderAddress string, toAddress string, subject string, body string) gmail.Message {
+	// body以外に日本語を指定したい場合はエンコードが必要
+	fromStr := "From: =?UTF-8?B?" + base64.StdEncoding.EncodeToString([]byte(senderName)) + "?= <" + senderAddress + ">\n"
+	toStr := "To: " + toAddress + "\n"
+	subStr := "Subject: =?UTF-8?B?" + base64.StdEncoding.EncodeToString([]byte(subject)) + "?=\n"
+	contentType := "Content-Type: text/plain; charset=UTF-8\n"
+	bodyStr := "\n" + body
+
+	var message gmail.Message
+	// 全体をURLエンコードしてRawに入れる
+	message.Raw = base64.URLEncoding.EncodeToString([]byte(fromStr + toStr + subStr + contentType + bodyStr))
+	return message
 }
